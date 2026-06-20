a03446c79401640ca8a35d7cff3ffb1a3f117c1d
Cristian <cristianarieldelgado@gmail.com>
2026-06-19 23:59:36 -0300

    Week 0

---
+++ HackerRank/Week00/function2.py
@@ -0,0 +1,17 @@
+""" 
+The included code stub will read an integer, n, from STDIN.
+
+Without using any string methods, try to print the following:
+1,2,3...n
+
+Note that "..." represents the consecutive values in between.
+"""
+
+n = int(input("N: "))
+
+i = 1
+
+while i <= n:
+    print(i)
+    i+=1
+
