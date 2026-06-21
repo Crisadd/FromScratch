c568d36eb6cde28fc97ef0caa6a3026d6bed46b4
Cristian <cristianarieldelgado@gmail.com>
2026-06-20 21:36:26 -0300

    day 3 Challenge

---
+++ 30DaysChallenge/Day3.py
@@ -0,0 +1,16 @@
+""" 
+Objective
+In this challenge, you will work with arithmetic operators. Check out the Tutorial tab for learning materials and an instructional video.
+
+Task
+Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent 
+(the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost. Round the result to the nearest 
+integer.
+
+Example
+mealCost = 100
+TipPercent = 15
+taxPercent = 8
+
+A tip of 15% * 100 = 15, and the taxes are 8% * 100 = 8. Print the value 123 and return from the function.
+"""
