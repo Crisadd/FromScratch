18390a051cc37de39593190e510408b5a7702d83
Cristian <cristianarieldelgado@gmail.com>
2026-06-24 21:23:33 -0300

    Week 2 loops, harvard

--- HarvardX-CS50P/week02 - Loops/cat.py
+++ HarvardX-CS50P/week02 - Loops/cat.py
@@ -3,10 +3,49 @@
 while i < 3:
     print('meow')
     i+=1
-"""
+
 
 for i in [0,1,2]:
     print('meow')
 
 
-    Conferencia: 20 minutos
+for i in range (3):     # Pithonic   for _ in range 
+    print('meow')
+
+
+print('meow\n' *3, end='')
+
+
+while True:
+    n = int(input("What's n? "))
+    if n < 0:
+        continue
+    else:
+        break
+
+
+while True:
+    n = int(input("What's n? "))
+    if n > 0:
+        break
+
+for _ in range(n):
+    print('meow')
+"""
+def main():
+    number = get_number()
+    meow(number)
+
+def get_number():
+    while True:
+        n = int(input("What's n? "))
+        if n > 0:
+            return n
+
+def meow(n):
+    for _ in range(n):
+        print('meow')
+
+
+
+main()
---
+++ HarvardX-CS50P/week02 - Loops/hogwarts.py
@@ -0,0 +1,22 @@
+# LIST
+
+students = ["Hermaione", "Harry", "Ron"]
+
+""" 
+print(students[0])
+print(students[1])
+print(students[2])
+
+for student in students:
+    print(student)
+
+
+"""
+
+for i in range(len(students)):  # Range don't take a string, take a number that's why i use lend()
+    print(i + 1, students[i])
+
+
+    minuto 44 termine el video
+
+    
