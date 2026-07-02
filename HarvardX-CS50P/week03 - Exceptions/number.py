75cd71252d17dbd53ab35880c776b49a7d2410db
Cristian <cristianarieldelgado@gmail.com>
2026-07-02 11:58:45 -0300

    Harvard - exceptions

---
+++ HarvardX-CS50P/week03 - Exceptions/number.py
@@ -0,0 +1,13 @@
+while True:
+    try: 
+        x = int(input("What's x? "))
+
+    except ValueError:
+        print('x is not an integer')
+
+    else:
+        break
+
+print(f'x is {x}')
+
+TERMINE EN 27:14
